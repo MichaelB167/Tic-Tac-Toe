5a6d965812f43cfeb65f17e5a03123fd582e0428
MichaelB167 <michael.m.bates@gmail.com>
2016-04-07 09:18:55 -0400

    Fix handler identifiers

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,19 +6,19 @@ const authApi = require('./api');
 const authUi = require('./ui');
 
 const addHandlers = () => {
-  $('#sign-up').on('submit', function (event) {
+  $('#signUpModal').on('submit', function (event) {
     let data = getFormFields(this);
     //get data, prevents default
     event.preventDefault();
     authApi.signUp(authUi.success, authUi.failure, data);
   });
-$('#sign-in').on('submit', function (event) {
+$('#signInModal').on('submit', function (event) {
     let data = getFormFields(this);
     //get data, prevents default
     event.preventDefault();
     authApi.signIn(authUi.success, authUi.failure, data);
   });
-$('#sign-out').on('submit', function (event) {
+$('#signOutModal').on('submit', function (event) {
       //get data, prevents default
       event.preventDefault();
       authApi.signOut(authUi.success, authUi.failure);
